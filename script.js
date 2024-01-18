13d22b1009d0572cd10e5eee6f6981c457f134ff
santosh <santoshpasi6566@gmail.com>
2024-01-18 22:04:31 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const API = "https://api.github.com/users/";
+let url = null;
+let currentPage = 1;
+const perPage = 10;
+
+const getUser = async () => {
+    const user = document.getElementById("input").value;
+    const fetchRepo = await fetch(API + user);
+    const totalRepo = await fetchRepo.json();
+    const pages = Math.ceil(totalRepo.public_repos/10); 
+    // totalRepo.forEach(repo => {
+        document.getElementById("url").innerHTML=`<p>${totalRepo.url}</p>`
+        const image = document.getElementById("image");
+        image.innerHTML = `<img src=${totalRepo.avatar_url}/>`
+        const detail = document.getElementById("details");
+        detail.innerHTML = `<h1>${totalRepo.name}</h1>
+        <p>${totalRepo.bio}</p>
+        <p>${totalRepo.location}</p>
+        <p>${totalRepo.twitter_username}</p>`
+    // });
+ 
+    const Repo = await fetch(API + user + "/repos" + `?per_page=${perPage}&page=${currentPage}`);
+    const data = await Repo.json();
+    displayRepositories(data);
+}
+
+function displayRepositories(repositories) {
+    const repositoriesContainer = document.getElementById('repositories');
+    repositoriesContainer.innerHTML = '';
+
+    repositories.forEach(repo => {
+        const repoLink = document.createElement('a');
+        repoLink.href = repo.html_url; 
+        repoLink.target = "_blank";
+        repoLink.innerHTML = `<h3>${repo.name}</h3>`;
+
+        const repoElement = document.createElement('div');
+        repoElement.classList.add('repository');
+        repoElement.appendChild(repoLink);
+
+        const descriptionElement = document.createElement('p');
+        descriptionElement.textContent = repo.description || "No description available.";
+        repoElement.appendChild(descriptionElement);
+
+        const languageButton = document.createElement('button');
+        languageButton.textContent = repo.language || "Not specified";
+        repoElement.appendChild(languageButton);
+
+        repositoriesContainer.appendChild(repoElement);
+    });
+}
+
+
+
+// ========================================================================================
+
