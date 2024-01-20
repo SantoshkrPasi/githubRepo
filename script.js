a2e21967586a443f8bff56920c5be2a83c920ed6
santosh <santoshpasi6566@gmail.com>
2024-01-20 23:31:03 +0530

    second commit

--- script.js
+++ script.js
@@ -2,24 +2,38 @@ const API = "https://api.github.com/users/";
 let url = null;
 let currentPage = 1;
 const perPage = 10;
+let repoArr = [];
+let cnt = 0;
+let particular_page = null;
+let totalpages = null;
 let repositoriesContainer = document.getElementById('repositories');
 
+
+const getPrev = () => {
+//  repoArr.forEach((item)=>{
+//  repositoriesContainer.removeChild(item);
+//  })
+   currentPage = currentPage > 1 ? currentPage - 1 : 1;
+   getUser(currentPage);
+}
+
 const getNext = () =>{
-repoArr.forEach((item)=>{
-  repositoriesContainer.removeChild(item);
+// repoArr.forEach((item)=>{
+//   repositoriesContainer.removeChild(item);
   
-})
-currentPage += 1;
-console.log(object)
-console.log(repoArr , "check")
+// })
+// currentPage += 1;
+currentPage = currentPage < totalpages ? currentPage + 1 : totalpages;
+getUser(currentPage);
 }
 
 
-const getUser = async () => {
+const getUser = async (current) => {
     const user = document.getElementById("input").value;
     const fetchRepo = await fetch(API + user);
     const totalRepo = await fetchRepo.json();
-    const pages = Math.ceil(totalRepo.public_repos/10); 
+    totalpages = Math.ceil(totalRepo.public_repos/10)
+        // const totalpages = Math.ceil(totalRepo.public_repos); 
     // totalRepo.forEach(repo => {
         document.getElementById("url").innerHTML=`<p>${totalRepo.url}</p>`
         const image = document.getElementById("image");
@@ -31,13 +45,15 @@ const getUser = async () => {
         <p>${totalRepo.twitter_username}</p>`
     // });
  
-    const Repo = await fetch(API + user + "/repos" + `?per_page=${perPage}&page=${currentPage}`)
+    const Repo = await fetch(API + user + "/repos" + `?per_page=${perPage}&page=${current}`)
     const data = await Repo.json();
     displayRepositories(data);
+    createRequiredPage(totalpages);
+    // window.location.reload();
 }
  
 
-let repoArr = [];
+
 function displayRepositories(repositories) {
    
     repositoriesContainer.innerHTML = '';
@@ -68,11 +84,32 @@ function displayRepositories(repositories) {
 
 }
 
-const number_of_page = document.getElementById("page-shift-container");
+
+
+const createRequiredPage = (totalpages) => {
+    const pageShiftContainer = document.getElementById("page-shift-container");
+    let num = parseInt(totalpages);
+    if(cnt == 0)
+    {        
+        for (let i = 1; i <= num; ++i) {
+            const listItem = document.createElement("li");
+            listItem.classList.add(`list`);
+            listItem.textContent = i;
+            pageShiftContainer.appendChild(listItem);
+            listItem.addEventListener("click", handleListItemClick);
+        }
+    }
+    cnt++;
+}
 
 
 
 
 
-
-
+function handleListItemClick(event) {
+    event.target.style.backgroundColor = "blue";
+    const innertext = event.target.innerText;
+    getUser(innertext);
+    currentPage = innertext;
+}
+  
